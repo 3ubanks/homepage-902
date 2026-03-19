bf524cddf66a248490dd66a8a79d55338dbb3512
3ubanks <eubanksle@vcu.edu>
2026-03-19 17:21:02 -0400

    ya

---
+++ jquery/main.js
@@ -0,0 +1,12 @@
+$(".dark-btn").click(
+    function(){
+        $(".box").toggleClass("dark");
+        $(".container").toggleClass("dark");
+
+});
+
+$(".spin-btn").click(
+    function(){
+        $(".box").toggleClass("dark");
+    }
+);
